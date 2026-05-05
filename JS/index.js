47086ad52175f896d7b7f8757d6f259e6d47d938
namuru16 <253505553+namuru16@users.noreply.github.com>
2026-05-06 05:23:07 +0900

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -9,19 +9,12 @@ window.addEventListener('load', function(){
 });
 
 $(document).ready(function() {
-	if ($(window).width() >=768){
 	$('#multiscroll').multiscroll({
-		responseWidth: 768,
 		easing:'easeInQuart'
-	});
-	}else{
-		$('body').addClass('ms-responsive');
-	}
 });
 
 $('#multiscroll').multiscroll({
 	licenseKey: 'OPEN-SOURCE-GPLv3-LICENSE',
 	verticalCentered: true,
-	responsiveWidth: 768,
 	touchSensitivity: 30
 });

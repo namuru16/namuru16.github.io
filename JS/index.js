67ef254dd3b4761befe798e803774b274184a4cc
namuru16 <253505553+namuru16@users.noreply.github.com>
2026-05-05 21:40:21 +0900

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+
+
+/*ロード画面が３秒で消える*/
+window.addEventListener('load', function(){
+	setTimeout(function(){
+		const loader = document.getElementById('Loading');
+		loader.classList.add('loaded');
+	},3000);
+});
+
+$(document).ready(function() {
+	$('#multiscroll').multiscroll({
+		responseWidth: 768,
+		easing:'easeInQuart'
+	});
+});
+
+$('#multiscroll').multiscroll({
+	licenseKey: 'OPEN-SOURCE-GPLv3-LICENSE',
+	verticalCentered: true
+});

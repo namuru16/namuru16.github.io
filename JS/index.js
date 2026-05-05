3cccf184a932debdcdbb388cb15471f2765d10fd
namuru16 <253505553+namuru16@users.noreply.github.com>
2026-05-06 08:46:55 +0900

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,18 @@
 window.addEventListener('load', function(){
+    //  multiscroll 初期化
+    $('#multiscroll').multiscroll({
+        licenseKey: 'OPEN-SOURCE-GPLv3-LICENSE',
+        verticalCentered: true,
+        scrollingSpeed: 1000,
+        touchSensitivity: 80,
+        easing: 'easeInQuart'
+    });
+
+    // ローディング画面を消す
     setTimeout(function(){
-        // 1. ローディング画面を非表示にする
         const loader = document.getElementById('Loading');
         if (loader) {
             loader.classList.add('loaded');
         }
-
-        // 2. multiscroll を初期化する
-        $('#multiscroll').multiscroll({
-            licenseKey: 'OPEN-SOURCE-GPLv3-LICENSE',
-            verticalCentered: true,
-            scrollingSpeed: 1000,
-            touchSensitivity: 80, // スワイプの誤検知を防ぐ
-            easing: 'easeInQuart'
-        });
-
     }, 3000); 
 });
